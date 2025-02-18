5aa03a0a48ff5dfcfd3b371cdeb7bba3a2a5b3c0
Davitoris1 <davidaladzeme717@gmail.com>
2025-02-17 23:25:12 -0500

    removing the bugs

--- node_2d.gd
+++ node_2d.gd
@@ -1,36 +1,3 @@
 extends Node2D
 
-
-const OBJECT_SCENE: PackedScene = preload("res://node_2d.tscn")
-var slimeTexture = preload("res://icon.svg")
-@onready var spawn_timer =  get_node("spawn_timer")
-
-	
-	
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	
-	add_child(spawn_timer)
-	#print("Timer created and added to scene")
-	
-	spawn_timer.connect("timeout", Callable(self, "_on_spawn_timer_timeout"))
-	#print("Timer connected to the 'timeout' sginal.")
-	
-func _on_spawn_timer_timeout() -> void:
-	#print("Timer timeout triggered.")
-	spawnSlime(Vector2(randf_range(0,1000),300))
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	pass
-		
-
-
-func spawnSlime(spawn_position: Vector2) -> void:
-	var new_object = OBJECT_SCENE.instantiate() 
-	var sprite = Sprite2D.new()
-	sprite.texture = slimeTexture
-	new_object.position = spawn_position
-	new_object.add_child(sprite)
-	add_child(new_object)
-	#print("Slime added to scene tree")
+pass
