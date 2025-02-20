d1725c5b6cae77c8e577a8d9d6673af0152ed362
Davitoris1 <davidaladzeme717@gmail.com>
2025-02-20 01:10:05 -0500

    label and class updates

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -1,3 +1,5 @@
+class_name Player 
+
 extends CharacterBody2D
 
 
@@ -10,18 +12,20 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if $"../Control/Label".visible==false:
+			
+		# Handle jump.
+		if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+			velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	
-	velocity.x = SPEED * direction
-	if Input.get_axis("ui_left", "ui_right"):
-		direction = Input.get_axis("ui_left", "ui_right")
-	
-	#Input.get_axis("ui_left", "ui_right")
-	#	velocity.x = move_toward(velocity.x, 300, SPEED)
+		# Get the input direction and handle the movement/deceleration.
+		# As good practice, you should replace UI actions with custom gameplay actions.
+		
+		velocity.x = SPEED * direction
+		if Input.get_axis("ui_left", "ui_right"):
+			direction = Input.get_axis("ui_left", "ui_right")
+		
+		#Input.get_axis("ui_left", "ui_right")
+		#	velocity.x = move_toward(velocity.x, 300, SPEED)
 
-	move_and_slide()
+		move_and_slide()
